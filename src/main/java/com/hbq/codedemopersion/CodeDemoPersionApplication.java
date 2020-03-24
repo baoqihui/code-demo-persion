2174596a9aad337cbea5893260d6f5e488f10e8c
huibaoqi <1318168098@qq.com>
2020-03-24 13:34:10 +0800

    含阿里oss和支付宝支付

---
+++ src/main/java/com/hbq/codedemopersion/CodeDemoPersionApplication.java
@@ -0,0 +1,13 @@
+package com.hbq.codedemopersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDemoPersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDemoPersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hbq/codedemopersion/CodeDemoPersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbq.codedemopersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDemoPersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
