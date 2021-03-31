66a372fa06bd1775a6c39be438426a88ef9de008
hui <1318168098@qq.com>
2021-03-31 17:47:05 +0800

    删除无用代码

--- src/main/java/com/hbq/codedemopersion/mapper/UmsUserMapper.java
+++ src/main/java/com/hbq/codedemopersion/mapper/UmsUserMapper.java
@@ -2,11 +2,9 @@ package com.hbq.codedemopersion.mapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hbq.codedemopersion.common.mapper.SuperMapper;
 import com.hbq.codedemopersion.model.UmsUser;
-import com.hbq.codedemopersion.vo.UmsPermissionVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,8 +22,6 @@ public interface UmsUserMapper extends SuperMapper<UmsUser> {
      * @return
      */
     Page<UmsUser> findList(Page<UmsUser> pages, @Param("p") Map<String, Object> params);
-
-    List<UmsPermissionVO> selectPermissionOfUser(@Param("userId") Long userId);
 
     int updateToNull(Long id);
 }
