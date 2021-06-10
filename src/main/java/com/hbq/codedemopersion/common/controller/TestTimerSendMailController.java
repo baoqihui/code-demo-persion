27282aa710709f833a91a79651f94da4d678007c
hui <1318168098@qq.com>
2021-06-10 14:17:12 +0800

    postman接口测试文档

--- src/main/java/com/hbq/codedemopersion/common/controller/TestTimerSendMailController.java
+++ src/main/java/com/hbq/codedemopersion/common/controller/TestTimerSendMailController.java
@@ -1,25 +1,28 @@
-package com.hbq.codedemopersion.common.controller;
+package com.yk.i_wms.common.controller;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.extra.mail.MailUtil;
+import cn.hutool.http.HttpUtil;
 import com.hbq.codedemopersion.common.model.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
+/**
+ * @Author: hbq
+ * @Description: 定时发送邮件测试
+ * @Date: 2021/3/26 15:13
+ */
 @Slf4j
 @CrossOrigin
 @RestController
@@ -37,39 +40,33 @@ public class TestTimerSendMailController {
      *每周星期六凌晨1点实行一次：0 0 1?*L
      *在26分、29 分、33 分执行一次：0 26,29,33***?
      *每天的0点、13 点、18 点、21 点都执行一次：0 0 0,13,18,21**?
+     *  [秒] [分] [小时] [日] [月] [周] [年]
+     * 序号 	说明 	必填 	允许填写的值 	    允许的通配符
+     * 1 	秒 	    是 	    0-59 	        , - * /
+     * 2 	分 	    是 	    0-59 	        , - * /
+     * 3 	时 	    是 	    0-23 	        , - * /
+     * 4 	日 	    是 	    1-31 	        , - * ? / L W
+     * 5 	月 	    是 	    1-12/JAN-DEC 	, - * /
+     * 6 	周 	    是 	    1-7 or SUN-SAT 	, - * ? / L #
+     * 7 	年 	    否 	    1970-2099 	    , - * /
      *
      * */
-    @Scheduled(cron ="0,30,59 20 16 * * *")
+    @Scheduled(cron ="0 0 0 26-31 5 2021")
     @ApiOperation(value = "发送邮件测试")
     @PostMapping("/mail/send")
     public Result send( ){
         ArrayList<String> tos = CollUtil.newArrayList(
-                "idse666666@163.com",
-                "1697253894@qq.com",
-                "1315751089@qq.com",
-                "2442069168@qq.com");
-        String oneS = getOneS();
+                "2442069168@qq.com",
+                "1318168098@qq.com"
+        );
+        Date date = DateUtil.parseDate("2021-06-03 00:00:00");
+        long day = DateUtil.between(new Date(), date, DateUnit.DAY);
+        String oneS ="大哥您好！温馨提醒：你的回爸爸距生日还有"+day+"天;请您及时准备祝福或礼物！" +
+                "<br> 此外：你将会在6月3日之前的每天晚上0点收到此邮件(￣３￣)a  爱您！" +
+                "<br> <img src='http://img.huijia21.com/blog/1622021097878.png'";
+        String s = HttpUtil.get("https://chp.shadiao.app/api.php");
         System.out.println(oneS);
-        //MailUtil.send(tos, "彩虹屁测试", oneS, false);
+        MailUtil.send(tos, "大哥您好！", oneS, true);
         return Result.succeed("保存成功");
-    }
-
-    public static   String getOneS(){
-        try {
-            //创建客户端对象
-            HttpClient client = HttpClients.createDefault();
-            /*创建地址 https://du.shadiao.app/api.php*/
-            HttpGet get = new HttpGet("https://chp.shadiao.app/api.php");
-            //发起请求，接收响应对象
-            HttpResponse response = client.execute(get);
-            //获取响应体，响应数据是一种基于HTTP协议标准字符串的对象
-            //响应体和响应头，都是封装HTTP协议数据。直接使用可能出现乱码或解析错误
-            HttpEntity entity = response.getEntity();
-            //通过HTTP实体工具类，转换响应体数据
-            String responseString = EntityUtils.toString(entity, "utf-8");
-            return responseString;
-        } catch (IOException e) {
-            throw  new RuntimeException("网站获取句子失败");
-        }
     }
 }
