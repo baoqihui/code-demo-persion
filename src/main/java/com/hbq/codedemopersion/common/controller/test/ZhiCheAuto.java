635561f52e29a5019a6dba5bfdd19a64daeb4d86
hui <1318168098@qq.com>
2021-12-10 13:53:16 +0800

    1.多数据源的配置 2.redis工具类修改 3.异步请求Async测试 4.hutool测试 5.es相关测试 6.微信小程序相关测试

---
+++ src/main/java/com/hbq/codedemopersion/common/controller/test/ZhiCheAuto.java
@@ -0,0 +1,14 @@
+package com.hbq.codedemopersion.common.controller.test;
+
+
+import lombok.Data;
+
+@Data
+/**
+ * 微信回调消息
+ */
+public class ZhiCheAuto {
+    private String username;
+    private String plateNo;
+    private String plateType;
+}
